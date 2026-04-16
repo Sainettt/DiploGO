15cd1e21a1eec8bb9483d34a1e0d133d232eeecd
Danyil Fiut <danyilfiut@gmail.com>
2026-04-16 21:19:45 +0200

    many new tables in db, base tables for statistic

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,11 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Only allow defined properties in DTOs
-    forbidNonWhitelisted: true, // Throw an error if extraneous properties are provided
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Only allow defined properties in DTOs
+      forbidNonWhitelisted: true, // Throw an error if extraneous properties are provided
+    }),
+  );
 
   app.enableCors(); // Allow requests from our frontend
   await app.listen(process.env.PORT ?? 4000); // 4000 to match api.client.ts
